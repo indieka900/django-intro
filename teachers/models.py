2e7ac2123e9091d02f46bd9a2ac957e3795772ae
Joseph <indiekaj@gmail.com>
2023-10-26 16:58:33 +0300

    Creaated models

--- teachers/models.py
+++ teachers/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    age = models.IntegerField()
+    image = models.ImageField(upload_to='students', default='profile.png')
+
 # Create your models here.
--- teachers/views.py
+++ teachers/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import HttpResponse,render
+from django.shortcuts import render
 
 
 def home(request):
